6d90466d4196825ddf5b6ae019a3144ace84acf3
chaseb1985 <chasebarrier85@gmail.com>
2019-01-09 16:09:10 -0600

    route working

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const express = require ('express');
 
 const app = express();
 
+// watch for imcoing requests for method GET
+//to the route http://localhost:3050/api
+app.get('/api', (req, res) =>{
+    res.send({ hi:'there' });
+});
+
+
 module.exports = app;
