4888103860279343c8e0981c71d3998c77b3e009
chaseb1985 <chasebarrier85@gmail.com>
2019-01-10 12:50:03 -0600

    troubleshoot

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require ('express');
 const routes = require('./routes/routes');
+const mongoose = require('mongoose');
 const app = express();
 const bodyPerser = require('body-parser');
 //Parse incoming request bodies in a middleware before your handlers, 
 //available under the req.body property.
 
+mongoose.Promise = global.Promise;
+//fixes depracation warning
+
+mongoose.connect('mongodb://localhost/mongopractice');
+//this connects my database using mongoose
 app.use(bodyPerser.json());
 routes(app);
 
