1824f7e91aa78f3d93ef26b611bc83c405a62c7e
chaseb1985 <chasebarrier85@gmail.com>
2019-01-10 12:20:17 -0600

    driver model

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require ('express');
 const routes = require('./routes/routes');
 const app = express();
+const bodyPerser = require('body-parser');
+//Parse incoming request bodies in a middleware before your handlers, 
+//available under the req.body property.
 
-
-
+app.use(bodyPerser.json());
 routes(app);
 
 
